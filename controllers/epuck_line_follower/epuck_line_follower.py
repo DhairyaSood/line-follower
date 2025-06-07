0c09ae50efae75a9730ef9dbc2bfd19aca8c6cec
DhairyaSood <dhairyasood20042006@gmail.com>
2025-06-07 16:27:57 +0530

    updated the controller to use 9 ir sensors, added textures for aa realistic arena image

--- controllers/epuck_line_follower/epuck_line_follower.py
+++ controllers/epuck_line_follower/epuck_line_follower.py
@@ -13,55 +13,50 @@ fast_speed = 3.0
 medium_speed = 1.5
 slow_speed = 0.5
 
-sensor_names = ['ir0', 'ir3', 'ir4', 'ir2', 'ir1']
-sensors = []
-for name in sensor_names:
-    sensor = robot.getDevice(name)
+# ir1 (far right) to ir9 (far left)
+sensor_names = ['ir9', 'ir8', 'ir7', 'ir6', 'ir5', 'ir4', 'ir3', 'ir2', 'ir1']
+sensors = [robot.getDevice(name) for name in sensor_names]
+for sensor in sensors:
     sensor.enable(time_step)
-    sensors.append(sensor)
 
 def black_line(value):
-    if value > 5:
-        return True
-    else:
-        return False
+    return value > 5
 
 while robot.step(time_step) != -1:
-    sensor_values = []
-    for sensor in sensors:
-        sensor_values.append(sensor.getValue())
+    sensor_values = [sensor.getValue() for sensor in sensors]
+    sensor_states = [1 if black_line(val) else 0 for val in sensor_values]
 
-    sensor_states = []
-    for val in sensor_values:
-        if black_line(val):
-            sensor_states.append(1)
-        else:
-            sensor_states.append(0)
-
-    print("Sensor readings:", sensor_values)
+    print("Sensor readings:", [round(v, 2) for v in sensor_values])
     print("Line detected:", sensor_states)
 
-    left_far = sensor_states[0]    # ir0
-    left_mid = sensor_states[1]    # ir3
-    center = sensor_states[2]      # ir4
-    right_mid = sensor_states[3]   # ir2
-    right_far = sensor_states[4]   # ir1
+    # sensor_states[0] is ir9 (leftmost), sensor_states[8] is ir1 (rightmost)
+    left_far = sensor_states[0]
+    left_mid_far = sensor_states[1]
+    left_mid = sensor_states[2]
+    center_left = sensor_states[3]
+    center = sensor_states[4]
+    center_right = sensor_states[5]
+    right_mid = sensor_states[6]
+    right_mid_far = sensor_states[7]
+    right_far = sensor_states[8]
 
-    if center == 1:
+    # Priority-based movement
+    if center:
         left_motor.setVelocity(fast_speed)
         right_motor.setVelocity(fast_speed)
-    elif left_mid == 1:
+    elif center_left or left_mid:
         left_motor.setVelocity(medium_speed)
         right_motor.setVelocity(fast_speed)
-    elif right_mid == 1:
+    elif center_right or right_mid:
         left_motor.setVelocity(fast_speed)
         right_motor.setVelocity(medium_speed)
-    elif left_far == 1:
+    elif left_mid_far or left_far:
         left_motor.setVelocity(slow_speed)
         right_motor.setVelocity(fast_speed)
-    elif right_far == 1:
+    elif right_mid_far or right_far:
         left_motor.setVelocity(fast_speed)
         right_motor.setVelocity(slow_speed)
     else:
+        # Stop if no line is detected
         left_motor.setVelocity(0)
         right_motor.setVelocity(0)
